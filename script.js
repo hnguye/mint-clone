63d24f25e2d273eb624fe6158999f76249f2e100
hnguye <hieutnguyen06@gmail.com>
2020-08-03 22:12:53 +1000

    Dynamically generate dates

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-if (true) {
-    do this
-}
+const footer = document.querySelector('.main-footer');
+const copyright = document.createElement('div');
+const date = new Date;
+
+copyright.innerHTML =
+    `<div>
+    <p>
+        © ${date.getFullYear()} Intuit, Inc. All rights reserved. Intuit, QuickBooks, QB,
+        TurboTax, ProConnect and Mint are registered trademarks of
+        Intuit Inc. </p>
+    <p>
+         Terms and conditions, features, support, pricing,
+        and service options subject to change without notice.
+    </p>
+    <div>`
+
+
+footer.append(copyright)
